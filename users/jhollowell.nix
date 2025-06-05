0972d4bc073167b58dd114ec0c62b9779451b5d8
James Hollowell <jamesthollowell@gmail.com>
2025-06-05 13:55:31 -0400

    feat: add work laptop

--- users/jhollowell.nix
+++ users/jhollowell.nix
@@ -10,6 +10,7 @@ in {
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMHlvLsl5o1Ln9s7QVqz5eFbymS8Xp34VrznbnOZ789m jhollowell@jth-gaming-desktop"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP9RPOtkTMqjPR/3LX+XJZfVwtVZAM7NkbGgK+gIZMMN jhollowell@jhollowell-pop-laptop"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMbvXdNDaNhmLQTRZzSU6t6l9HJUVhtLkz4T2j4r6+z4 jhollowell@jhollowell-frmwk"
+        "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBB4fzSF0vl9vmZe5TBqTfEEHjiMGV1C2a4mzuFqWQsn3bwOGGJ8RDk4LuwiwmW2pGrm7TdUTbMEazj9D5jLhle8= swampfox\jhollowell@jhollowell9520"
       ];
     };
 
